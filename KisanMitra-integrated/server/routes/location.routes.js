b329ce7265682fe813fdaf78e8598b2d8d39508b
Shreyansh2025 <shreyanshsurana100@gmail.com>
2026-07-06 12:18:30 +0530

    Remove duplicate nested folder from bad merge

--- KisanMitra-integrated/server/routes/location.routes.js
+++
@@ -1,9 +0,0 @@
-// routes/location.routes.js
-const router = require("express").Router();
-const { requireLocation }     = require("../middleware/location.middleware");
-const { getLocationSummary }  = require("../controllers/location.controller");
-
-// GET /api/location-summary?lat=&lon=&state=
-router.get("/", requireLocation, getLocationSummary);
-
-module.exports = router;
--- KisanMitra-integrated/server/routes/weather.routes.js
+++
@@ -1,9 +0,0 @@
-// routes/weather.routes.js
-const router = require("express").Router();
-const { requireLocation } = require("../middleware/location.middleware");
-const { getWeather }      = require("../controllers/weather.controller");
-
-// GET /api/weather?lat=&lon=&state=
-router.get("/", requireLocation, getWeather);
-
-module.exports = router;
